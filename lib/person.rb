69ec633e32506aea6b056d19933f706d75b63780
Flatiron School <flatironschool@Flatirons-MacBook-Air-96.local>
2018-11-26 19:45:05 -0500

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,99 +1,89 @@
 require 'pry'
 
-class Person
-  attr_reader :name, :hygiene
-attr_accessor :bank_account, :happiness
 
+class Person
+
+  attr_reader :name, :bank_account, :happiness, :hygiene 
+  attr_writer :bank_account
   def initialize(name)
-    @name = name
+    @name = name 
     @bank_account = 25
     @happiness = 8
     @hygiene = 8
   end
-
-
-  def happiness=(happiness)
-    @happiness = happiness
-      if @happiness > 10
-        @happiness = 10
-      end
-      if @happiness < 0
-        @happiness = 0
-      end
+  
+  def happiness=(arg)
+    @happiness = arg
+    if @happiness > 10 
+      @happiness = 10
+    elsif 
+      @happiness < 0 
+      @happiness = 0
+    end
   end
-
-  def hygiene=(hygiene)
-    @hygiene = hygiene
-      if @hygiene > 10
-        @hygiene = 10
-      end
-      if @hygiene < 0
-        @hygiene = 0
-      end
-  end
-
-  def clean?
-    if  @hygiene > 7
-      return true
-    else
-      return false
+  
+  def hygiene=(arg)
+    @hygiene = arg
+    if @hygiene > 10 
+      @hygiene = 10
+    elsif 
+      @hygiene < 0 
+      @hygiene = 0
     end
   end
 
-  def happy?
-    if  @happiness > 7
-      return true
+  def happy? 
+    if @happiness > 7 
+      return true 
     else
-      return false
-    end
+      false 
+    end 
+  end
+  
+  def clean? 
+    if @hygiene > 7 
+      return true 
+    else
+      false 
+    end 
   end
 
   def get_paid(salary)
     @bank_account += salary
-    return "all about the benjamins"
+    "all about the benjamins"
+    
   end
 
   def take_bath
     self.hygiene += 4
-
-  #  if @hygiene > 10
-  #    @hygiene = 10
-  #  end
-    return "♪ Rub-a-dub just relaxing in the tub ♫"
+    "♪ Rub-a-dub just relaxing in the tub ♫"
   end
 
+  def work_out 
+     self.hygiene -= 3
+     self.happiness += 2
+     "♪ another one bites the dust ♫"
+  end
 
-  def work_out
-
-    self.happiness += 2
-    self.hygiene -= 3
-
-
-    return "♪ another one bites the dust ♫"
-
-end
-
-  def call_friend(friend)
-    self.happiness += 3
+  def call_friend(friend) 
     friend.happiness += 3
-
-return "Hi #{friend.name}! It's #{@name}. How are you?"
-
+    self.happiness += 3
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
   end
 
-    def start_conversation(friend, topic)
-      if topic == "politics"
-        self.happiness -= 2
-        friend.happiness -= 2
-      return "blah blah partisan blah lobbyist"
-
-      elsif topic == "weather"
-        self.happiness += 1
-        friend.happiness += 1
-      return "blah blah sun blah rain"
-
-      else
-      return "blah blah blah blah blah"
+  def start_conversation(person,topic) 
+    case topic 
+      when "politics"
+        person.happiness -= 2 
+        self.happiness -=2 
+        'blah blah partisan blah lobbyist'
+      when "weather"
+        person.happiness += 1 
+        self.happiness += 1 
+        'blah blah sun blah rain'
+      else 
+        "blah blah blah blah blah"
     end
   end
+
 end
